273694a58b045c3b2536b79908c572b15cf512b0
Syarifudin <suicideudin@gmail.com>
2023-02-19 13:25:06 +0700

    Commit SpringBoot Tugas Praktikum 2 Soal 2

---
+++ src/main/java/com/juaracoding/msajspringbootjpa/repo/ProvinsiRepo.java
@@ -0,0 +1,15 @@
+package com.juaracoding.msajspringbootjpa.repo;/*
+Created By IntelliJ IDEA 2022.3.2 (Ultimate Edition)
+Build #IU-223.8617.56, built on January 26, 2023
+@Author Syarifudin a.k.a. Muhamad Syarifuidn
+Java Developer
+Created on 19/02/2023 8:42
+@Last Modified 19/02/2023 8:42
+Version 1.1
+*/
+
+import com.juaracoding.msajspringbootjpa.model.Provinsi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinsiRepo extends JpaRepository<Provinsi,Long> {
+}
